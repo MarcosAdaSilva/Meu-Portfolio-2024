00be1e3aa841df8603107dd1d970b070eae6a370
Marcos Antônio da Silva <marcaofly@gmail.com>
2024-05-02 22:47:08 -0300

    Subindo arquivos para o GitHub

---
+++ main.js
@@ -0,0 +1,11 @@
+var Typed = new Typed(".text", {
+  strings: [
+    "Desenvolvedor Frontend",
+    "Desenvolvedor Backend",
+    "Desenvolvedor Web",
+  ],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
